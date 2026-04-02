3f4ffa8ce0cbbe35fb0ef154b8a444bca1b6d6f5
nishanthvonteddu <nishanthreddyw@gmail.com>
2026-04-02 14:29:41 -0400

    fix(day-01): update backend/src/core/middleware.py

--- backend/src/core/middleware.py
+++ backend/src/core/middleware.py
@@ -2,13 +2,13 @@ import time
 from uuid import uuid4
 
 import structlog
-from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
 
 
 class RequestLoggingMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next) -> Response:
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         request_id = request.headers.get("x-request-id", str(uuid4()))
         structlog.contextvars.clear_contextvars()
         structlog.contextvars.bind_contextvars(request_id=request_id)
